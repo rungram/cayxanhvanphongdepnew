8fd18328f43dcfdafb1f01138ce115b9f0600bb0
kcdagtemyatpxh <nguyenleduykhang29111994@gmail.com>
2017-09-18 09:58:30 +0700

    update

--- templates/lienhe_tpl.php
+++ templates/lienhe_tpl.php
@@ -1,3 +1,8 @@
+<?php $ngonngu = (!empty($_COOKIE['ngonngu']))?$_COOKIE['ngonngu']:'vn';?>
+<?php
+if($ngonngu == 'vn')
+{ 
+?>
 <?php 
 if( $_SERVER['REQUEST_METHOD'] == 'POST' )
 {
@@ -72,3 +77,83 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' )
                     </div>
                 </div>
             </div>
+<?php 
+}
+else 
+{
+?>
+<?php 
+if( $_SERVER['REQUEST_METHOD'] == 'POST' )
+{
+	global $d;
+	$data['ten'] = $_POST['ten'];
+	$data['email'] = $_POST['email'];
+	$data['dienthoai'] = $_POST['dienthoai'];
+	$data['noidung'] = $_POST['noidung'];
+	$data['subject'] = $_POST['subject'];
+	$d->setTable('lienhe');
+	$d->insert($data);
+}
+?>
+<div id="wrapper">
+                <div class="">
+                    <div id="pathway" class="clearfix">
+                        <div class="container">
+                            <div id="breadcrumbs" itemscope="" itemtype="http://schema.org/BreadcrumbList">
+
+                                <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                                    <a href="index.html" itemprop="item"><span itemprop="name" class="hidden-xs">Home</span></a><meta itemprop="position" content="1">
+                                </span> 	
+                                <span class="hidden-xs">→</span>
+                                <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                                    <a href="san-pham.html" itemprop="item"><span itemprop="name">Contact</span></a>
+                                    <meta itemprop="position" content="2">
+                                </span>
+                            </div><!-- #breadcrumbs -->
+                        </div>
+                    </div>
+                    <div class="container">
+                            
+                           <div class="box-heading">
+                                        <h4 class="title">Contact</h4>
+                           </div>
+                           <div class="row border mar10">     
+
+                                <div class="col-md-6 col-sm-6">
+                                	<div class="contact-info">
+                                        You can send us a contact by completing the form below. We will reply to your letter, please complete the declaration. Pleased to serve and sincerely thank for your interest and contribution to us
+                                    </div>
+                                    <div class="divider"></div>
+                                    <div class="contact-form">
+                                    <form onsubmit="return alert('Send contact successfully');" method="post" name="frm" action="lien-he.html">
+                                        <p class="full-row">
+                                            <label for="name-id">Full name:</label>
+                                            <input type="text" id="name-id" name="ten" required="required">
+                                        </p>
+                                        <p class="full-row">
+                                            <label for="email-id">Email:</label>
+                                            <input type="text" id="email-id" name="email" required="required">
+                                        </p>
+                                        <p class="full-row">
+                                            <label for="subject-id">Topic:</label>
+                                            <input type="text" id="subject-id" name="subject" required="required">
+                                        </p>
+                                        <p class="full-row">
+                                            <label for="message">Content:</label>
+                                            <textarea name="noidung" id="message" rows="6" required="required"></textarea>
+                                        </p>
+                                        <input class="btn btn-success" name="" value="Send" type="submit">
+                                    </form>
+                                    </div>
+                                </div> <!-- /.col-md-3 -->
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="map-holder">
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4660.7224179809555!2d106.7364605895438!3d10.797155984931884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175260e3aeacb03%3A0x883aa75516f521e6!2zNzEgTmd1eeG7hW4gUXXDvSDEkOG7qWMsIEtodSDEkcO0IHRo4buLIEFuIFBow7ogQW4gS2jDoW5oLCBBbiBQaMO6LCBRdeG6rW4gMiwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1504879244477" width="550" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                    </div> <!-- /.map-holder -->
+                                    
+                                </div> <!-- /.col-md-3 -->
+                            </div>
+                    </div>
+                </div>
+            </div>
+<?php }?>
--- templates/thanhtoancotk_tpl.php
+++ templates/thanhtoancotk_tpl.php
@@ -1,3 +1,8 @@
+<?php $ngonngu = (!empty($_COOKIE['ngonngu']))?$_COOKIE['ngonngu']:'vn';?>
+<?php
+if($ngonngu == 'vn')
+{ 
+?>
 <?php  if(!defined('_source')) die("Error");
 {
 	$d->reset();
@@ -153,3 +158,164 @@
                     </div>
                 </div>
             </div>
+<?php 
+}
+else 
+{
+?>
+<?php  if(!defined('_source')) die("Error");
+{
+	$d->reset();
+	$sql_tungdanhmuc="select id,thumb,ten_$lang,tenkhongdau,gia,masp,giagiam,spmoi,luotxem from #_product where spdc >0  order by stt asc ";
+	$d->query($sql_tungdanhmuc);	
+	$result_spnam=$d->result_array();	
+	
+
+	
+				
+	$curPage = isset($_GET['p']) ? $_GET['p'] : 1;
+	$url=getCurrentPageURL();
+	$maxR=20;
+	$maxP=5;
+	$paging=paging_home($result_spnam , $url, $curPage, $maxR, $maxP);
+	$result_spnam=$paging['source'];
+	
+}
+?>
+<div id="wrapper">
+                <div class="">
+                    <div id="pathway" class="clearfix">
+                        <div class="container">
+                            <div id="breadcrumbs" itemscope="" itemtype="http://schema.org/BreadcrumbList">
+
+                                <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                                    <a href="http://chohoaviet.com" itemprop="item"><span itemprop="name" class="hidden-xs">Home</span></a><meta itemprop="position" content="1">
+                                </span> 	
+                                <span class="hidden-xs">→</span>
+                                <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                                    <a href="san-pham.html" itemprop="item"><span itemprop="name">Payment</span></a>
+                                    <meta itemprop="position" content="2">
+                                </span>
+                            </div><!-- #breadcrumbs -->
+                        </div>
+                    </div>
+                    <div class="container">
+                            
+                           <div class="box-heading">
+                                        <h4 class="title">Payment</h4>
+                           </div>
+                           <div id="thanhtoan">
+                        <form method="post" name="frm" action="gui-don-hang.html" enctype="multipart/form-data">
+                        <div class="row mart-t50">
+                            <div class="col-md-6 col-sm-6">
+                                <div class="contact-form">
+                                    <p class="full-row">
+                                        <label class="col-lg-5" for="name-id">Name (*):</label>
+                                        <span class="col-lg-7 form-group"><input id="name-id" name="hoten2" type="text" required></span>
+                                    </p>
+                                    <p class="full-row">
+                                        <label class="col-lg-5" for="email-id">Email (*):</label>
+                                        <span class="col-lg-7 form-group"><input id="email-id" name="email" type="text" required></span>
+                                    </p>
+                                    <p class="full-row">
+                                        <label class="col-lg-5" for="subject-id">Phone (*):</label>
+                                        <span class="col-lg-7 form-group"><input id="subject-id" name="dienthoai" type="text" required></span>
+                                    </p>
+                                    <p class="full-row">
+                                        <label class="col-lg-5" for="message">Address (*):</label>
+                                        <span class="col-lg-7 form-group"><input id="add-id" name="diachi" type="text" required></span>
+                                    </p>
+                                    <p class="full-row">
+                                        <label class="col-lg-5" for="message">Note</label>
+                                         <span class="col-lg-7 form-group"><textarea style="width: 100%;" name="noidung" type="text" id="add-id" ></textarea></span>
+                                    </p>
+                                </div>
+                            </div> <!-- /.col-md-6 -->
+                            <div class="col-md-6 col-sm-6">
+                                <div class="table-responsive" id="no-more-tables">
+                                    <table class="table table-hover table-vcenter table-bordered">
+                                        <tbody>
+                                         <?php
+                            $max=count($_SESSION['cart']);
+                            $ship = 0;
+                            $vat = 0;
+                            	for($i=0;$i<$max;$i++)
+                            	{
+                            		
+                            					$pid=$_SESSION['cart'][$i]['productid'];
+                            					$q=$_SESSION['cart'][$i]['qty'];					
+                            					$pname=get_product_name_en($pid);
+                            					$phinh=get_hinh($pid);
+                            					$pmota=get_mota($pid);
+                            					$pkodau=get_kodau($pid);
+                            					$psale=get_giagiam($pid);
+                            					$pma=get_masp($pid);
+                            					if($psale ==0)
+                            					{
+                            					    $gia = get_price($pid);
+                            					}
+                            					else
+                            					{
+                            					    $gia = $psale;
+                            					}
+                            					$ship = $ship+$q*get_phivanchuyen($pid);
+                            					$vat = $vat + $gia*$q*get_vat($pid)/100;
+                            		?> 
+                                <tr class="row-1193"> 
+                                    <td colspan="2" class="text-left"> 
+                                        <div class="media"> 
+                                            <div class="media-left"> 
+                                                <a href="chi-tiet-san-pham/<?=$pkodau?>-<?=$pid?>.html"> <img class="small-img" src="upload/sanpham/<?=$phinh?>" alt=""> </a> 
+                                            </div> 
+                                            <div class="media-body"> 
+                                                <h4 class="media-heading"> <a href="chi-tiet-san-pham/<?=$pkodau?>-<?=$pid?>.html" title="<?=$pname?>">  <?=$pname?></a> <br> Product code: <strong><?=$pma?></strong> </h4> <p> <strong class="lblTotalPrice">
+                                                <?php 
+					echo number_format($gia,0, ',', '.').'₫ x '.$q;
+					 ?>=<?php echo number_format($gia*$q,0, ',', '.').'₫';?>  </strong> </p> 
+                                          </div>
+                                        </div> 
+                                    </td>
+                                </tr>
+                                <?php
+                                	}
+                                ?>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr> 
+                                     <td colspan="2" class="text-right text-primary" style="white-space:nowrap;font-weight:700;">  Money: <strong class="lblTotal" data-total="180000"><?= number_format( get_ordersale_total($pid)+$row_setting['phivc'],0, ',', '.').'đ';?></strong> 
+                                     </td>
+                                 </tr>
+                                 <tr> 
+                                     <td colspan="2" class="text-right text-primary" style="white-space:nowrap;font-weight:700;">V.A.T: <strong class="lblTotal" data-total="180000">
+                                     <?php  
+                                     echo number_format($vat,0, ',', '.').'₫';
+                                     ?></strong> 
+                                     </td>
+                                 </tr>
+                                 <tr class="ship-price"> 
+                                     <td colspan="2" class="text-right text-info" style="white-space:nowrap;font-weight:700;">  Shipping Charge: <strong class="lblShipPrice" data-price="0">
+                                     <?php  
+                                     echo number_format($ship,0, ',', '.').'₫';
+                                     ?></strong> 
+                                       <input name="Ship" id="Ship" value="0" type="hidden"> </td>
+                                 </tr>
+                                 <tr class="ship-price"> 
+                                     <td colspan="2" class="text-right text-success">  Total: <strong class="lblTotalPrice"><?= number_format( get_ordersale_total($pid)+$ship + $vat,0, ',', '.').'đ';?></strong> 
+                                     </td>
+                                 </tr>
+
+
+
+                                        </tfoot>
+                                    </table>
+                                </div>
+                                <p class="clearfix"></p>
+                                <a><button class="btn btn-cart"> <i class=" fa fa-cart-plus"></i> Order  </button></a>
+                            </div> <!-- /.col-md-6 -->
+                        </div>
+                    </form>
+                    </div>
+                    </div>
+                </div>
+            </div>
+<?php }?>
